85b465807df0778a4b7a73a2e57c9f146719c2a8
Juliane Blier <blier.juliane@gmail.com>
2016-10-21 11:49:43 +0200

    Version fonctionnelle du programme What a Day

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 (function(){
 	'use strict';
 	var app = {
+		dayOfWeek: null,
 		weekDays: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
 		data: {},
 		moment: null,
@@ -9,32 +10,46 @@
 		},
 		listeners: function(){
 			$('#check').on('click', this.runCheck.bind(this));
+			$('#dayOfWeek').on('click', '#restart', this.undisplay.bind(this));
 		},
 		getData: function(){
-			var date = parseInt($('#date').val());
-			var month = parseInt($('#month').val());
-			var year = parseInt($('#year').val());
 			this.data = {
-				year: year,
-				month: month,
-				day: date
-			}
-		},
-		getDay: function(){
-			var dayOfWeek = (this.moment).day();
-			console.log(this.weekDays[dayOfWeek]);
-		},
-		runCheck: function(){
-			this.checkDate();
-			this.checkYear();
-			if(this.checkDate() && this.checkYear()){
-				this.getData();
-				this.transformMoment();
-				this.getDay();
+				year: parseInt($('#year').val()),
+				month: parseInt($('#month').val()),
+				day: parseInt($('#date').val())
 			}
 		},
 		transformMoment: function(){
 			this.moment = moment(this.data);
+		},
+		getDay: function(){
+			this.dayOfWeek = this.weekDays[this.moment.day()];
+		},
+		runCheck: function(){
+			this.undisplay();
+			if(this.checkDate() && this.checkYear()){
+				this.getData();
+				this.transformMoment();
+				this.getDay();
+				this.displayDay();
+			}
+		},
+		displayDay: function(){
+			$('#dayOfWeek').addClass('overlay');
+			if(this.dayOfWeek === undefined){
+				$('#dayOfWeek').html('<h1>Jour Inexistant</h1><button id="restart" class="restart">Recommencer</button>');
+			}else {	
+				$('#dayOfWeek').html('<h1>' + this.dayOfWeek + '</h1><button id="restart" class="restart">Recommencer</button>');
+			}
+		},
+		undisplay: function(){
+			$('#message').removeClass('error');
+			$('#message').html('');
+			$('#year').css('border', '');
+			$('#date').css('border', '');
+
+			$('#dayOfWeek').removeClass('overlay');
+			$('#dayOfWeek').html('');
 		},
 		checkDate: function(){
 			if($('#date').val() > 0 && $('#date').val() <= 31){
@@ -54,12 +69,12 @@
 		},
 		errorFieldDate: function(){
 			$('#message').addClass('error');
-			$('#date').css('border', 'solid red 2px');
+			$('#date').css('border', 'solid #ff7473 2px');
 			$('#message').html('Le jour doit être compris entre 1 et 31 !');	
 		},
 		errorFieldYear: function(){
 			$('#message').addClass('error');
-			$('#year').css('border', 'solid red 2px');
+			$('#year').css('border', 'solid #ff7473 2px');
 			$('#message').html('L\'année doit être supérieure à 0 !');
 		}
 	}
